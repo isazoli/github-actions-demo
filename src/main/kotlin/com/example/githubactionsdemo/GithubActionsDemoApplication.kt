505c1b93ab4b39a673f16997b8f037b4170609dd
isazoli <zoltan@ji-sys.com>
2021-08-16 22:31:54 +0200

    add base files

---
+++ src/main/kotlin/com/example/githubactionsdemo/GithubActionsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubactionsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionsDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubActionsDemoApplication>(*args)
+}
